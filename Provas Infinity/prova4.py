f7fa603ef2ceb977eaa665b4ab5fb6456f9889f4
sevs1 <leonardo.pace.2007@gmail.com>
2025-06-13 23:56:55 -0300

    13/06/2025 - 23:57

---
+++ Provas Infinity/prova4.py
@@ -0,0 +1,5 @@
+lista = ['Pêssego','Acerola','Abacate','Melancia','Amora','Limão']
+
+tupla = tuple(lista)
+
+print(tupla)
--- teste.py
+++ teste.py
@@ -1,10 +1,22 @@
-'''tabuleiro = [
+'''
+tabuleiro = [
     [4, 2, 2],
     [4, 2, 4],
     [4, 0, 0]
 ]
 
-print(tabuleiro[0][1])'''
+print(tabuleiro[0][1])
+'''
 
+'''
 print('Hello World \f')
 print('Ola mundo')
+'''
+
+'''
+tupla = (1, 2, 3, 4, 5)
+
+lista = list(tupla)
+
+print(lista)
+'''
